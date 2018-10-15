d13ccdb6515cb7bf55ebf7a925da3249f8939339
Ken Lai <ken.lai@redso.com.hk>
2018-10-15 17:47:37 +0800

    Add constant for testnet

--- index.js
+++ index.js
@@ -15,18 +15,26 @@ let NETWORK_TYPE = typeforce.compile({
 });
 
 let BITCOIN = {
-  wif: 0x80,
+  messagePrefix: "\x18Bitcoin Signed Message:\n",
+  bech32: "bc",
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4
-  }
+  },
+  pubKeyHash: 0x00,
+  scriptHash: 0x05,
+  wif: 0x80
 };
 let TESTNET = {
-  wif: 0xef,
+  messagePrefix: "\x18Bitcoin Signed Message:\n",
+  bech32: "tb",
   bip32: {
     public: 0x043587cf,
     private: 0x04358394
-  }
+  },
+  pubKeyHash: 0x6f,
+  scriptHash: 0xc4,
+  wif: 0xef
 };
 
 function BIP32(d, Q, chainCode, network) {
